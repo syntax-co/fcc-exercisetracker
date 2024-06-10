24ffccf3e62a0d37e848c560e60b0c88ea668353
syntax-co <51330734+syntax-co@users.noreply.github.com>
2024-06-09 20:10:49 -0500

    init

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ app.get('/', (req, res) => {
 
 
 
+const exerciseSchema = {
+  username: String,
+  description: String,
+  duration: Number,
+  date: String,
+  _id: String
+}
+
+
+const userSchema = {
+  username: String,
+  _id: String
+}
+
+
+const logSchema = {
+  username: String,
+  count: Number,
+  _id: String,
+  log: [String]
+}
+// ^^^^^^
+// log:{
+//   description: "test",
+//   duration: 60,
+//   date: "Mon Jan 01 1990",
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
